341c56cf83433c4e804128b0e3f15529946cb321
korobasssss <an.korobeynikova@gmail.com>
2024-10-30 17:00:52 +0300

    change loader

--- src/base/components/Loader/Loader.tsx
+++ src/base/components/Loader/Loader.tsx
@@ -3,33 +3,21 @@ import loader_step2 from '../../../assets/loader_step2.svg'
 import loader_step3 from '../../../assets/loader_step3.svg'
 import loader_step4 from '../../../assets/loader_step4.svg'
 import { Icon } from '../'
-import { useEffect, useState } from 'react'
+import styles from './styles/styles.module.scss'
 
 export const Loader = () => {
     const allSteps = [loader_step1, loader_step2, loader_step3, loader_step4];
 
-    const [currentStep, setCurrentStep] = useState<string | null>(null);
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        setCurrentStep(allSteps[currentIndex]);
-    }, [currentIndex]);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentIndex(prevIndex => (prevIndex + 1) % allSteps.length);
-        }, 150);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
     return (
-        <>
-            {currentStep && (
-                <Icon
-                    icon={currentStep} 
-                />
-            )}
-        </>
-    );
+        <div className={styles.SLoader}>
+            {allSteps.map(icon => {
+                return (
+                    <Icon 
+                        classNames={styles.SItem}
+                        icon={icon}
+                    />
+                )
+            })}
+        </div>
+    )
 }
